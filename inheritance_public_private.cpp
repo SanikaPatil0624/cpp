d14dfab94a22595effe94f6c9a9531cb854f91aa
SanikaPatil5 <159874181+SanikaPatil0624@users.noreply.github.com>
2024-04-16 10:57:08 +0530

    Create inheritance_public_private.cpp

---
+++ inheritance_public_private.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class person{
+public:
+       int age=19;
+       string name="Sanika";
+       string add="Chandur;
+       long int mob_no =132453546;
+
+     void display()
+   {
+      cout<<age<<endl;
+      cout<<name<<endl;
+      cout<<add<<endl;
+      cout<<mob_no<<endl;
+      
+    }
+};
+
+class Employee : private person
+{
+public:
+       int id=1;
+       int salary=999000;
+      void show()
+      {
+      
+       cout<<id<<endl;
+       cout<<salary<<endl;
+       
+     }
+};
+
+class Student : public person
+{
+public:
+       int roll_no=315;
+       string std="SY CSE(AI)";
+       float marks=87.80;
+
+     void show1()
+    {
+     
+      cout<<roll_no<<endl;
+      cout<<std<<endl;
+      cout<<marks<<endl;
+      
+   }
+};
+
+
+int main()
+{
+     person p;
+     //p.display();
+     Employee emp;
+     cout<<"Employee INFO :"<<endl;
+     emp.display();
+     emp.show();
+     Student st;
+     cout<<"Student INFO :"<<endl;
+     st.display();
+     st.show1();
+
+return 0;
+}
+
